3590b8dc89a698e36f4c3f19919c48cfd4d0d1ff
Suzy <susan.m.mullin@gmail.com>
2018-10-08 01:43:06 -0700

    checkpoint-1-scaffolding-and-deployment: Completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 
 const routeConfig = require('./config/route-config.js');
 const appConfig = require('./config/main-config.js');
 
 appConfig.init();
 routeConfig.init(app);
+app.use(logger('dev'));
 
 module.exports = app;
